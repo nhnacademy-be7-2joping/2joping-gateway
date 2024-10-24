2c031eb7bd2382ac2ab95a47445a218f6f1b7603
sehui-ing <116075689+sehui-ing@users.noreply.github.com>
2024-10-24 17:16:36 +0900

    feat: init project

---
+++ src/main/java/com/nhnacademy/twojopinggateway/TwojopingGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojopinggateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwojopingGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwojopingGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nhnacademy/twojopinggateway/TwojopingGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nhnacademy.twojopinggateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwojopingGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
