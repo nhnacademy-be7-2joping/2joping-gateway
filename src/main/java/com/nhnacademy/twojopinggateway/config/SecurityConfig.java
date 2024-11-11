42865dcad67e657e7f7c601223a820a167523456
YeonWooKimm <kyw10987@gmail.com>
2024-11-11 16:11:03 +0900

    feat: Jwt

--- src/main/java/com/nhnacademy/twojopinggateway/config/SecurityConfig.java
+++ src/main/java/com/nhnacademy/twojopinggateway/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.nhnacademy.twojopinggateway.config;
 
+import com.nhnacademy.twojopinggateway.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableReactiveMethodSecurity;
@@ -11,6 +13,10 @@ import org.springframework.security.web.server.SecurityWebFilterChain;
 @EnableWebFluxSecurity
 @EnableReactiveMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
+
     @Bean
     public SecurityWebFilterChain securityFilterChain(ServerHttpSecurity http) throws Exception {
         http.csrf(ServerHttpSecurity.CsrfSpec::disable);
