531b31428c92e5ee0b7e1000b7b09c1512d7dd61
Sauter001 <lsj457@naver.com>
2024-11-21 12:20:19 +0900

    fix: add /logout to auth service

--- src/main/java/com/nhnacademy/twojopinggateway/config/GatewayConfig.java
+++ src/main/java/com/nhnacademy/twojopinggateway/config/GatewayConfig.java
@@ -1,6 +1,5 @@
 package com.nhnacademy.twojopinggateway.config;
 
-import com.nhnacademy.twojopinggateway.client.UserInfoClient;
 import com.nhnacademy.twojopinggateway.filter.CustomFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.gateway.route.RouteLocator;
@@ -18,11 +17,10 @@ public class GatewayConfig {
     public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
 
 
-
         CustomFilter.Config config = new CustomFilter.Config();
 
         return builder.routes()
-                .route("auth_service", r -> r.path("/auth/**", "/login")
+                .route("auth_service", r -> r.path("/auth/**", "/login", "/logout")
                         .filters(f -> f.filter(customFilter.apply(config)))
                         .uri("lb://TWOJOPING-USER-AUTH"))// fill service here...
                 .route("bookstore_service", r -> r.path("/api/v1/**")
